7243657e259a70828d69788ab7484e0a33800f74
yxqsnz <yxqsnz@gmail.com>
2022-08-03 22:47:01 -0300

    feat: add some infos to /about

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,8 +1,11 @@
 export default function About() {
   return (
     <section class="bg-pink-100 text-gray-700 p-8">
-      <h1 class="text-2xl font-bold">About</h1>
-      <p class="mt-4">Hi, I'm Moizes J. Sousa (aka yxqsnz)!</p>
+      <h1 class="text-2xl font-bold">About me</h1>
+      <p class="mt-4">Hi! My name is Moizes J. Sousa (aka yxqsnz), I have 13 years! </p>
+      <p class="mt-4">I am a software developer, I have a passion for programming and I love to learn new things.</p>
+      <p class="mt-4">I currently live in Brazil.</p>
+      <p class="mt-4">I am working in a project called Rio.</p>
     </section>
   );
 }
