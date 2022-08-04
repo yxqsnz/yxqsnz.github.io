1ac6f9065695175921d44300074549e7edc40ca4
yxqsnz <yxqsnz@gmail.com>
2022-08-03 22:24:44 -0300

    dist

--- src/pages/home.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <section class="bg-gray-100 text-gray-700 p-8 mx-auto">
-      <h1 class="text-2xl font-bold">Welcome to my website.</h1>
-      <p class="mt-4">A Young Developer.</p>
-
-    </section >
-  );
-}
