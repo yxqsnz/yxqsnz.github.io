5aea1fa147a91889562cd3da377af3c4155cb071
yxqsnz <yxqsnz@gmail.com>
2022-08-03 22:19:35 -0300

    feat: basic website

---
+++ src/pages/home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <section class="bg-gray-100 text-gray-700 p-8 mx-auto">
+      <h1 class="text-2xl font-bold">Welcome to my website.</h1>
+      <p class="mt-4">A Young Developer.</p>
+
+    </section >
+  );
+}
